9b150cdecae89ec74a1233d3a1a8814453b92e5b
Henrique Lima <barrosohenriquelima@gmail.com>
2022-10-26 19:06:39 -0300

    incluindo prop id

--- src/models/id.py
+++ src/models/id.py
@@ -2,7 +2,7 @@ from flask_restx import fields
 from src.server.instance import server
 
 id =  {
-    'id': fields.String(description='ID do registro'),
+    'id': fields.String(description='ID do registro', required=True),
 }
 
 id_request = server.api.model('Id', id )
