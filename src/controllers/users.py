64ca68bbd6792bab0969b919f1991bf177e7d5c8
Henrique Lima <barrosohenriquelima@gmail.com>
2022-11-26 11:44:10 -0300

    criacao user

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,7 +1,7 @@
 from flask_restx import Resource
 from src.server.instance import server
-from src.models.user import user_response
-
+from src.models.user import user_response, user_request, user_update_request
+from src.service.user import user_service
 
 app, api = server.app, server.api.namespace('users',
                                             description='Recurso de usuários')
@@ -9,4 +9,17 @@ app, api = server.app, server.api.namespace('users',
 class User(Resource):
     @api.marshal_list_with(user_response)
     def get(self):
-        return {'id': '1', 'nome': 'teste'}, 200
+        users = user_service.get()
+        return users, 200
+    
+    @api.expect(user_request, validate=True)
+    @api.marshal_with(user_response)
+    def post(self):
+        user = user_service.post(api.payload)
+        return user, 201
+    
+    @api.expect(user_update_request, validate=True)
+    @api.marshal_with(user_response)
+    def put(self):
+        response = user_service.put(api.payload)
+        return response, 204
