ffb40749513c32c7d451d79f87011a2ab9eec470
RobertLemos14 <robertlemos14@gmAIL.COM>
2023-02-01 19:36:01 -0300

    enums

--- src/models/enums.py
+++ src/models/enums.py
@@ -1,9 +1,2 @@
-from flask_restx import fields
-from src.server.instance import server
-from src.models.id import id
-
-
-
-unidade_enum = ['unidade', 'cartela', 'caixa', 'duzia', 'dezena', 'kilograma', 'grama', 'litro','Unidade', 'Cartela', 'Caixa', 'Duzia', 'Dezena', 'Kilograma', 'Grama', 'Litro']
-categoria_enum = ['fruta', 'legume', 'verdura', 'hortalicas','Fruta', 'Legume', 'Verdura', 'Hortalicas']
-
+unidade_enum = ['unidade', 'cartela', 'caixa', 'duzia', 'dezena', 'kilograma', 'grama', 'litro']
+categoria_enum = ['fruta', 'legume', 'verdura', 'hortalicas']
