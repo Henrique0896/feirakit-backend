1e8b395401ba1ef9781b16fd626297bf3d29dac8
RobertLemos14 <95971607+RobertLemos14@users.noreply.github.com>
2023-02-01 19:24:54 -0300

    Update enums.py

--- src/models/enums.py
+++ src/models/enums.py
@@ -1,3 +1,3 @@
 unidade_enum = ['unidade', 'cartela', 'caixa', 'duzia', 'dezena', 'kilograma', 'grama', 'litro']
-categoria_enum = ['fruta', 'legume', 'verdura', 'hortalicas'']
+categoria_enum = ['fruta', 'legume', 'verdura', 'hortalicas']
 
