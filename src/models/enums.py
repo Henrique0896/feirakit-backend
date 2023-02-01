d6282d8c08a3f2bba821f6394fd7641cd63b29c7
RobertLemos14 <robertlemos14@gmAIL.COM>
2023-02-01 19:37:30 -0300

    enums

--- src/models/enums.py
+++ src/models/enums.py
@@ -1,7 +1,2 @@
 unidade_enum = ['unidade', 'cartela', 'caixa', 'duzia', 'dezena', 'kilograma', 'grama', 'litro']
-<<<<<<< HEAD
 categoria_enum = ['fruta', 'legume', 'verdura', 'hortalicas']
-=======
-categoria_enum = ['fruta', 'legume', 'verdura', 'hortalicas']
-
->>>>>>> 1e8b395401ba1ef9781b16fd626297bf3d29dac8
