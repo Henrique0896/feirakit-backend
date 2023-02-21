11718b14729e34317a1e008d32f5c664227a5db1
Henrique Lima <henrique.lima@dtidigital.com.br>
2023-02-21 16:09:19 -0300

    config env

--- src/program/database.py
+++ src/program/database.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
+import os
 
 class Database():
     def __init__(self):
-        self.client = MongoClient('DATABASE')
+        self.client = MongoClient(os.environ['DATABASE'])
         self.main = self.client['feirakit-database']
         
 database = Database()
