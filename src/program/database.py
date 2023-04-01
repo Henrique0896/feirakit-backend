b06ce2ba01d809dfc8c40469d9daa73ae14ba20c
Daniel-MSoares <munizsoaresrapper@gmail.com>
2023-04-01 12:53:49 -0300

    cria variáveis de ambiente

--- src/program/database.py
+++ src/program/database.py
@@ -3,9 +3,11 @@ import os
 import dotenv
 dotenv.load_dotenv(dotenv.find_dotenv())
 
+
 class Database():
     def __init__(self):
         self.client = MongoClient(os.getenv("DB_CONNECTION"))
         self.main = self.client[os.getenv("DATABASE")]
-        
+
+
 database = Database()
--- src/program/instance.py
+++ src/program/instance.py
@@ -4,22 +4,24 @@ import os
 import dotenv
 dotenv.load_dotenv(dotenv.find_dotenv())
 
+
 class Server():
     def __init__(self):
         self.app = Flask(__name__)
         self.app.config.setdefault("RESTX_MASK_SWAGGER", False)
         self.app.config['SECRET_KEY'] = [os.getenv("SECRET_KEY")]
         self.api = Api(self.app,
-         version='1.0',
-         title='Feirakit API',
-         description='',
-         doc='/swagger'
-        )
-    
+                       version='1.0',
+                       title='Feirakit API',
+                       description='',
+                       doc='/swagger'
+                       )
+
     def run(self):
         self.app.run(
             debug=True,
             host='0.0.0.0', port=5000
         )
+
 
 server = Server()
