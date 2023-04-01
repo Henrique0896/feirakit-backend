4f88d0a9e57e47079d255ccf129d1469c96ecf32
Daniel-MSoares <munizsoaresrapper@gmail.com>
2023-04-01 12:48:12 -0300

    cria cariáveis de ambiente

--- src/program/database.py
+++ src/program/database.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
-
+import os
+import dotenv
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 class Database():
     def __init__(self):
-        self.client = MongoClient('DATABASE')
-        self.main = self.client['feirakit-database']
+        self.client = MongoClient(os.getenv("DB_CONNECTION"))
+        self.main = self.client[os.getenv("DATABASE")]
         
 database = Database()
