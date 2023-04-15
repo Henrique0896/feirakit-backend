712e9639c61f0e5e07294d6cb4bd14c0b2f804dc
Henrique Lima <henrique.lima@dtidigital.com.br>
2023-04-14 22:05:38 -0300

    correcao nome var

--- src/program/database.py
+++ src/program/database.py
@@ -3,6 +3,6 @@ from src.core.settings_env import settings_env
 class Database():
     def __init__(self):
         self.client = MongoClient(settings_env.get_var("DB_CONNECTION"))
-        self.main = self.client[settings_env.get_var("DATABASE2")]
+        self.main = self.client[settings_env.get_var("DATABASE")]
 
 database = Database()
