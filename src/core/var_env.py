1405de7edb564d8c140f2a47e76ac39ef0a78cab
Henrique Lima <henrique.lima@dtidigital.com.br>
2023-04-17 08:42:09 -0300

    make a redeploy

--- src/core/var_env.py
+++ src/core/var_env.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 
-class VarEnv():
+class VarEnvs():
     def __init__(self):
         load_dotenv(find_dotenv())
         self.secret_key = getenv("SECRET_KEY")
         self.database = getenv("DATABASE")
         self.db_connection = getenv("DB_CONNECTION")
 
-var_env = VarEnv()
+var_env = VarEnvs()
